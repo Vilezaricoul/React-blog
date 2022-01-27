dcac84ea54011a1a0f88f06e57a7cdfb280a2434
Vilezaricoul <demonlaplas7@gmail.com>
2022-01-27 21:22:11 +0300

    commit

---
+++ src/components/UI/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React, {useContext} from 'react';
+import {Link} from "react-router-dom";
+import MyButton from "../button/MyButton";
+import {AuthContext} from "../../../context";
+
+const Navbar = () => {
+    const {isAuth, setIsAuth} = useContext(AuthContext);
+
+    const logout = () => {
+        setIsAuth(false);
+        localStorage.removeItem('auth')
+    }
+
+    return (
+        <div className="navbar">
+            <MyButton onClick={logout}>
+                Выйти
+            </MyButton>
+            <div className="navbar__links">
+			<MyButton><Link to="/about">О сайте</Link></MyButton>
+            <MyButton><Link to="/posts">Посты</Link></MyButton>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
